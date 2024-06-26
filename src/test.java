c6239cf99c2fb01ea4a716ab34b499dceed593dd
nanami-tomoe <npnp123npnp123@gmail.com>
2024-06-26 23:57:08 +0900

    test

--- src/test.java
+++ src/test.java
@@ -1,2 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class test {
+    public static void main(String[] args) {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            String input = bufferedReader.readLine();
+            String[] inputArray = input.split(", ");
+
+            int maxValue = Integer.MIN_VALUE;
+            List<Integer> inputList = new ArrayList<>();
+            List<Integer> indices = new ArrayList<>();
+
+            for (String str : inputArray) {
+                inputList.add(Integer.parseInt(str));
+                if (Integer.parseInt(str) > maxValue) {
+                    maxValue = Integer.parseInt(str);
+                }
+            }
+
+            for (int i = 0; i < inputList.size(); i++) {
+                if (inputList.get(i) == maxValue) {
+                    indices.add(i);
+                }
+            }
+
+            System.out.println(indices);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
